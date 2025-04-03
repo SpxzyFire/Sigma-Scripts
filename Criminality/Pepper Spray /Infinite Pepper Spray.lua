4a57ec8bd303ebdf054a31f0ae4955ff874f0151
SpxzyFire <eren.bouf@gmail.com>
2025-04-03 09:58:21 -0500

    Update Infinite Pepper Spray.lua

--- Criminality/Pepper Spray /Infinite Pepper Spray.lua
+++ Criminality/Pepper Spray /Infinite Pepper Spray.lua
@@ -1,1 +1,19 @@
+local PepperSpray = {
+    InfiniteSpray = false
+}
 
+game:GetService("RunService").RenderStepped:Connect(function()
+    wait(1)
+    if PepperSpray.InfiniteSpray and game.Players.LocalPlayer.Character:FindFirstChild("Pepper-spray") then
+        game.Players.LocalPlayer.Character["Pepper-spray"].Ammo.Value = 99
+        game.Players.LocalPlayer.Character["Pepper-spray"].RemoteEvent:FireServer("Update", 99)
+    end
+end)
+
+GunModsTab:AddToggle({
+	Name = "Infinite Pepper Spray",
+	Default = false,
+	Callback = function(Value)
+        PepperSpray.InfiniteSpray = Value
+	end    
+})
