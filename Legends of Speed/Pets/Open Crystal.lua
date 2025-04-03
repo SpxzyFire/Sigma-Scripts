e0ee9d42d94b244e0a4d15a69c1303a81a1d08b7
SpxzyFire <eren.bouf@gmail.com>
2025-04-03 12:43:13 -0500

    Create Open Crystal.lua

---
+++ Legends of Speed/Pets/Open Crystal.lua
@@ -0,0 +1,63 @@
+local crystalNames = {
+    "Red Crystal",
+    "Blue Crystal",
+    "Purple Crystal",
+    "Lightning Crystal",
+    "Snow Crystal",
+    "Lava Crystal",
+    "Inferno Crystal",
+    "Electro Legends Crystal",
+    "Jungle Crystal"
+}
+
+local selectedCrystal = ""
+
+Crystals:AddDropdown({
+    Name = "Dropdown",
+    Default = "Pick the Crystal you wish to buy",
+    Options = crystalNames,
+    Callback = function(Value)
+        selectedCrystal = Value
+        print("Selected crystal: " .. selectedCrystal)
+    end    
+})
+
+Crystals:AddButton({
+    Name = "Buy Crystal",
+    Callback = function()
+        if selectedCrystal == "" then
+            print("No crystal selected!")
+            return
+        end
+
+        local ohString1 = "openCrystal"
+        local ohString2 = selectedCrystal
+        
+        game:GetService("ReplicatedStorage").rEvents.openCrystalRemote:InvokeServer(ohString1, ohString2)
+        print("Buy Crystal button pressed for: " .. selectedCrystal)
+    end    
+})
+
+
+Crystals:AddToggle({
+    Name = "Mass Buy Crystal",
+    Default = false,
+    Callback = function(Value)
+        autoBuyCrystal = Value
+        if autoBuyCrystal then
+            print("Mass buy enabled for: " .. selectedCrystal)
+            while autoBuyCrystal do
+                if selectedCrystal ~= "" then
+                    local ohString1 = "openCrystal"
+                    local ohString2 = selectedCrystal
+                    
+                    game:GetService("ReplicatedStorage").rEvents.openCrystalRemote:InvokeServer(ohString1, ohString2)
+                    print("Mass buying crystal: " .. selectedCrystal)
+                end
+                wait(0.1)
+            end
+        else
+            print("Mass buy disabled.")
+        end
+    end    
+})
